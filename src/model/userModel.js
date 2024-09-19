ca34bd1705277d3299d0428dcabebf6dfc2f8f1d
Ssouza001 <souzarennato001@gmail.com>
2024-09-19 20:59:38 -0300

    Criei tabela usúario
    Modelo do banco de dados

---
+++ src/model/userModel.js
@@ -0,0 +1,70 @@
+const mongoose = require("mongoose");
+
+const UsuarioSchema = new mongoose.Schema({
+    nome: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 50,
+
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 50,
+        unique: true,  // para garantir que não existam e-mails duplicados
+        lowercase: true,  // para armazenar em minúsculas
+        validate: [validator.isEmail, 'Invalid email address']
+    },
+    senha: {
+        type: String,
+        required: true,
+        minlength: 8,  // comprimento mínimo para aumentar a segurança
+    },
+    descricao: {
+        type: String,
+        required: true,  // Pode ser obrigatório dependendo da aplicação
+        maxlength: 150   // Limite de caracteres, se necessário
+    },
+    departamento: {
+        type: String,
+        required: true
+    },
+    sala: {
+        type: String,
+        required: true
+    },
+    nota: {
+        type: Number,
+        required: true,
+        min: 0,   // Valor mínimo
+        max: 10   // Valor máximo, por exemplo
+    },
+    nota_comunicacao: {
+        type: Number,
+        required: true,
+        min: 0,   // Valor mínimo
+        max: 10   // Valor máximo, por exemplo
+    }, 
+    nota_agilidade: {
+        type: Number,
+        required: true,
+        min: 0,   // Valor mínimo
+        max: 10   // Valor máximo, por exemplo
+    }, 
+    nota_equipe: {
+        type: Number,
+        required: true,
+        min: 0,   // Valor mínimo
+        max: 10   // Valor máximo, por exemplo
+    }, 
+    nota_assiduidade: {
+        type: Number,
+        required: true,
+        min: 0,   // Valor mínimo
+        max: 10   // Valor máximo, por exemplo
+    }
+});
+
+module.exports = mongoose.model('Usuarios', UsuarioSchema);
